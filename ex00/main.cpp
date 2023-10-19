bdc23c33852fa4872720fd390c570d623345ea8c
hossine eddahbi <eddahbi.hossine.solicode@gmail.com>
2023-10-19 21:20:44 +0100

    create mode

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -3,4 +3,12 @@
 int main()
 {
     std::string str = "HI THIS IS BRAIN";
+
+    std::string *ptr = &str;
+    std::string &ref = str;
+
+    std::cout << "str: " << str << std::endl;
+    std::cout << "ptr: " << *ptr << std::endl;
+    std::cout << "ref: " << ref << std::endl;
+    return 0;
 }
